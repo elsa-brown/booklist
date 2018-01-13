8cff07a841d4107e7585738895727b15b0c8931b
Elsa Brown <elsambrown@gmail.com>
2018-01-13 00:03:18 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+import ChannelsListWithData from './components/ChannelsListWithData';
+import NotFound from './components/NotFound';
+import ChannelDetails from './components/ChannelDetails';
+
+import { ApolloClient, ApolloProvider, createNetworkInterface, toIdValue } from 'react-apollo';
+
+
+const networkInterface = createNetworkInterface({ uri: 'http://localhost:4000/graphql' });
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    setTimeout(next, 500);
+  },
+}]);
+
+// tags GraphQL objects in the ApolloClient cache
+function dataIdFromObject (result) {
+  if (result.__typename) {
+    if (result.id !== undefined) {
+      return `${result.__typename}:${result.id}`;
+    }
+  }
+  return null;
+}
+
+const client = new ApolloClient({
+  networkInterface,
+  customResolvers: {
+    Query: {
+      channel: (_, args) => {
+        // toIdValue ensures an ID type is returned
+        return toIdValue(dataIdFromObject({ __typename: 'Channel', id: args['id'] }))
+      }
+    }
+  },
+  dataIdFromObject,
+});
+
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <div className="App">
+            <Link to="/" className="navbar">React + GraphQL Tutorial</Link>
+            <Switch>
+              <Route exact path="/" component={ChannelsListWithData}/>
+              <Route path="/channel/:channelId" component={ChannelDetails}/>
+              <Route component={ NotFound }/>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/ChannelDetails.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import MessageList from './MessageList';
+import ChannelPreview from './ChannelPreview';
+import NotFound from './NotFound';
+
+import { gql, graphql } from 'react-apollo';
+
+const ChannelDetails = ({ data: { loading, error, channel }, match }) => {
+  
+  if (loading) {
+    // custom resolver loads ChannelPreview (title) from cache, while messages are loading 
+    return <ChannelPreview channelId={match.params.channelId} />
+  } if (error) {
+    return <p>{ error.message }</p>
+  } if (channel === null) {
+    return <NotFound />
+  }
+
+  return (
+    <div>
+      <div className="channelName">
+        {channel.name}
+      </div>
+      <MessageList messages={channel.messages}/>
+    </div>);
+}
+
+export const channelDetailsQuery = gql`
+  query ChannelDetailsQuery($channelId: ID!) {
+    channel(id: $channelId) {
+      id
+      name
+      messages {
+        id
+        text
+      }
+    }
+  }
+`;
+
+const ChannelDetailsWithQuery = graphql(channelDetailsQuery, {
+  options: (props) => ({ variables: { channelId: props.match.params.channelId }
+  })
+});
+
+export default(ChannelDetailsWithQuery)(ChannelDetails);
